c1bac4d6410447021a4b7de254f0f00cc7f779d7
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-11-11 12:52:15 +0530

    got form data+ added thanku file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 print(__name__)
@@ -9,31 +9,40 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/works")
-def works():
-    return render_template("works.html")
+# dynamic routes!
+@app.route("/<name>")
+def all(name):
+    return render_template(name + ".html")
 
 
-@app.route("/work")
-def work():
-    return render_template("work.html")
+@app.route("/submit", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("./thanks")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# @app.route("/works")
+# def works():
+#     return render_template("works.html")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# @app.route("/work")
+# def work():
+#     return render_template("work.html")
 
 
-@app.route("/components")
-def components():
-    return render_template("components.html")
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 
 
-@app.route("/baap")
-def hello_world1():
-    return "Nannu ZindaBaad!"
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
+
+
+# @app.route("/components")
+# def components():
+#     return render_template("components.html")

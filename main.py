f03898d71e4f8508355fe82f6cf2a706d496024b
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-11-11 20:32:41 +0530

    added csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -15,12 +16,36 @@ def all(name):
     return render_template(name + ".html")
 
 
+def write_data(data):
+    with open("database.txt", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n {email},{subject},{message}")
+
+
 @app.route("/submit", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
         data = request.form.to_dict()
-        print(data)
+        write_data(data)
         return redirect("./thanks")
+
+
+def write_to_csv(data):
+    with open("database.csv", newline="", mode="a") as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(
+            csv,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL,
+            newline="",
+        )
+        csv_writer.writeheader()
+        csv_writer.writerow([email, subject, message])
 
 
 # @app.route("/works")
